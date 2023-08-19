e8061f075d75f8b7c389b42dfdc22d58d9fec723
Marc Ho <marc.ho380@gmail.com>
2023-08-18 23:53:19 -0700

    Update interactive interface

--- main.go
+++ main.go
@@ -2,17 +2,49 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"poker-hands/utils"
 	"poker-hands/validation"
 	"strings"
 )
 
 func main() {
-	args := os.Args
+	fmt.Println("Input one or more poker hands for evaluation.")
+	fmt.Println("Each hand should be a comma-delimited string of 5 cards in the following format: `XX,XX,XX,XX,XX`.")
+	fmt.Println("The first character represents the rank from 2-9, T, J, Q, K, A.")
+	fmt.Println("The second character represents the suit as S (Spades), H (Hearts), D (Diamonds), C (Clubs).")
+	fmt.Println("e.g. 2H,3S,TD,6C,JH")
+	fmt.Println()
 
-	cards := strings.Split(args[1], ",")
-	fmt.Println(cards)
+	fmt.Println("Enter poker hand(s) and click return to enter more.")
+	fmt.Println("Enter `done` to start evaluating.")
+	fmt.Println()
 
-	// Validate input for hand length, card value
-	validation.Validate(cards)
+	var input string
+	fmt.Scanln(&input)
+	hands := [][]string{}
+
+	for input != "done" {
+		s := strings.Split(input, ",")
+
+		// Validate input for hand length, card value
+		validation.Validate(s)
+
+		// Add valid input to hands slice
+		hands = append(hands, s)
+
+		// Continue scanning for more input
+		input = ""
+		fmt.Scanln(&input)
+	}
+
+	fmt.Printf("\nAll inputs: %v\n\n", hands)
+	evaluate(hands)
+}
+
+func evaluate(input [][]string) {
+	for _, hand := range input {
+		// Transform into card structs
+		cards := utils.Transform(hand)
+		fmt.Println(cards)
+	}
 }
