e611d9e404caf941d9646bd4bd204db33d4adab9
Marc Ho <marc.ho380@gmail.com>
2023-08-20 19:30:44 -0700

    Sort cards by highest value after transformation

--- utils/card.go
+++ utils/card.go
@@ -1,20 +1,38 @@
 package utils
 
+import (
+	"poker-hands/validation"
+	"sort"
+)
+
 type Card struct {
-	Rank string
+	Rank int
 	Suit string
 }
 
+// Transform each string representation
+// of a card into a Card struct
 func Transform(input []string) []Card {
 	cards := []Card{}
 
 	for _, card := range input {
-		rank := card[0:1]
+		// Get the value of the card as an int
+		// mapped to the Ranks map in validator
+		rank := validation.Ranks[card[0:1]]
 		suit := card[1:]
 
 		s := Card{rank, suit}
 		cards = append(cards, s)
 	}
 
-	return cards
+	return sortCards(cards)
+}
+
+// Sort each hand in descending order by highest ranked card
+func sortCards(hand []Card) []Card {
+	sort.SliceStable(hand, func(i, j int) bool {
+		return hand[i].Rank > hand[j].Rank
+	})
+
+	return hand
 }
